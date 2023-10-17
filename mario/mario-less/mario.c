03ef411ba9c8eca8af5bb23a76c9ed7777563654
Julio Loch <106871296+julioloch@users.noreply.github.com>
2023-10-17 08:37:18 +0000

    Tue, Oct 17, 2023, 5:37 AM -03:00

--- mario/mario-less/mario.c
+++ mario/mario-less/mario.c
@@ -11,7 +11,7 @@ int main(void)
 
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < i; j++)
+        for (int j = 0; j <= i; j++)
         {
             printf("#");
         }
