a2a42a4e3e454041ea36aa70c10841fae253a96d
Julio Loch <106871296+julioloch@users.noreply.github.com>
2023-10-17 09:00:44 +0000

    Tue, Oct 17, 2023, 6:00 AM -03:00

---
+++ mario/mario-more/mario.c
@@ -0,0 +1,38 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height = 0;
+    int numberOfDots = 0;
+    while (height < 1 || height > 8)
+    {
+        height = get_int("Whats the height? ");
+    }
+
+    numberOfDots = height - 2;
+
+    for (int line = 0; line < height; line++)
+    {
+
+        for (int i = 0; i <= numberOfDots; i++)
+        {
+            printf(" ");
+        }
+        numberOfDots--;
+
+        for (int j = 0; j <= line; j++)
+        {
+            printf("#");
+        }
+
+        printf("  ");
+
+        for (int j = 0; j <= line; j++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}
