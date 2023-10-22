122b75704dc80b918720b7d7552e0209f7f8b0ed
Julio Loch <106871296+julioloch@users.noreply.github.com>
2023-10-22 01:14:33 +0000

    Sat, Oct 21, 2023, 10:14 PM -03:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,11 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int long creditCard = 0;
+
 int main(void)
 {
     do{
-        int long creditCard = get_int("Number: ");
+        creditCard = get_int("Number: ");
     }
-    while (creditCard < 0);
-    printf(%i, creditCard);
+    while (creditCard <= 0);
+    printf(%int long, creditCard);
 }
