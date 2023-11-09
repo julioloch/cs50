5b1810670d934aa1b45766130360100b9a0bb715
Julio Loch <106871296+julioloch@users.noreply.github.com>
2023-11-09 05:00:49 +0000

    Thu, Nov 9, 2023, 2:00 AM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,16 +1,18 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     string textInput = get_string("Text: ");
+    int position = 0;
     int lettersTotal = 0;
     int wordsTotal = 0;
     int sentencesTotal = 0;
     int textLenght = strlen(textInput);
 
-    while (textInput[] != "\0"){
+    while (textInput[position] != "\0"){
         for (int i = 0; i < textLenght; i++){
             if (tolower(textInput[i]) > 'a' && tolower(textInput[i]) < 'z'){
                 lettersTotal++;
