64d77f1484d33dd34240b894f748147884fd3563
Julio Loch <106871296+julioloch@users.noreply.github.com>
2023-11-09 04:53:14 +0000

    Thu, Nov 9, 2023, 1:53 AM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -4,13 +4,15 @@
 int main(void)
 {
     string textInput = get_string("Text: ");
+    int lettersTotal = 0;
     int wordsTotal = 0;
     int sentencesTotal = 0;
     int textLenght = strlen(textInput);
 
     while (textInput[] != "\0"){
         for (int i = 0; i < textLenght; i++){
-            
+            lettersTotal++;
+
         }
     }
     printf("%s\n", textInput);
