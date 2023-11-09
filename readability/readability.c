72b9a83a25da24dc73acd1d9d839fe6c9331b3e7
Julio Loch <106871296+julioloch@users.noreply.github.com>
2023-11-09 05:52:27 +0000

    Thu, Nov 9, 2023, 2:52 AM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,8 +1,8 @@
 #include <cs50.h>
 #include <ctype.h>
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
 
 int main(void)
 {
@@ -11,26 +11,37 @@ int main(void)
     int sentences = 0;
     int words = 1;
 
-    for (int i = 0, textLenght = strlen(text); i < textLenght; i++){
-        if (tolower(text[i]) >= 'a' && tolower(text[i]) <= 'z'){
+    for (int i = 0, textLenght = strlen(text); i < textLenght; i++)
+    {
+        if (tolower(text[i]) >= 'a' && tolower(text[i]) <= 'z')
+        {
             letters++;
-        } else if (text[i] == ' '){
+        }
+        else if (text[i] == ' ')
+        {
             words++;
-        } else if (text[i] == '.' || text[i] == '!' || text[i] == '?'){
+        }
+        else if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
             sentences++;
         }
     }
 
-    double L = ((double)letters / words) * 100;
-    double S = ((double)sentences / words) * 100;
+    double L = ((double) letters / words) * 100;
+    double S = ((double) sentences / words) * 100;
     double index = (0.0588 * L) - (0.296 * S) - 15.8;
     int grade = round(index);
 
-    if (grade < 1){
+    if (grade < 1)
+    {
         printf("Before Grade 1\n");
-    } else if (grade >= 16){
+    }
+    else if (grade >= 16)
+    {
         printf("Grade 16+\n");
-    } else {
+    }
+    else
+    {
         printf("Grade %i\n", grade);
     }
 }
