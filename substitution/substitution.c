11a044ae3541bef157a19a53a4493eba2cbbe742
Julio Loch <106871296+julioloch@users.noreply.github.com>
2023-11-10 06:23:06 +0000

    Fri, Nov 10, 2023, 3:23 AM -03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,6 +1,6 @@
 #include <cs50.h>
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 #include <string.h>
 
 int isKeyValid(string key);
@@ -10,29 +10,35 @@ int main(int argc, string argv[])
 {
     const string key = argv[1];
 
-    if ((argc == 1) || (argc > 2)){
+    if ((argc == 1) || (argc > 2))
+    {
         printf("Usage: ./substitution key\n");
         return 1;
     }
 
-    if (strlen(argv[1]) != 26) {
+    if (strlen(argv[1]) != 26)
+    {
         printf("Key must contain 26 characters.\n");
         return 1;
     }
 
-    if (isKeyValid(key)){
+    if (isKeyValid(key))
+    {
         string plaintext = get_string("plaintext: ");
         string ciphertext = encrypt(plaintext, key);
         printf("ciphertext: %s", ciphertext);
         printf("\n");
-    } else {
+    }
+    else
+    {
         printf("Invalid Key\n");
         return 1;
     }
     return 0;
 }
 
-int isKeyValid(string key){
+int isKeyValid(string key)
+{
 
     /*
     Checks if a key is valid. Return 1 if valid, 0 if invalid.
@@ -45,16 +51,22 @@ int isKeyValid(string key){
 
     char keyFreq[26] = {0};
 
-    for (int i = 0; i < 26; i++){
-        if (isalpha(key[i]) == 0){ // Check if is alphabetic
+    for (int i = 0; i < 26; i++)
+    {
+        if (isalpha(key[i]) == 0)
+        { // Check if is alphabetic
             return 0;
-        } else {
+        }
+        else
+        {
             keyFreq[tolower(key[i]) - 'a']++; // Counts how many times a letter has been used
         }
     }
 
-    for (int i = 0; i < 26; i++){
-        if(keyFreq[i] > 1){ // Checks if a letter has been used twice or more
+    for (int i = 0; i < 26; i++)
+    {
+        if (keyFreq[i] > 1)
+        { // Checks if a letter has been used twice or more
             printf("Key has two or more identical characters\n");
             return 0;
         }
@@ -63,23 +75,29 @@ int isKeyValid(string key){
     return 1;
 }
 
-string encrypt(string plaintext, string key){
+string encrypt(string plaintext, string key)
+{
 
     string ciphertext = plaintext;
 
-
-    for (int i = 0; plaintext[i] != '\0'; i++){
+    for (int i = 0; plaintext[i] != '\0'; i++)
+    {
         char c = plaintext[i];
-        if (isalpha(c)){
-            if (islower(c)){
+        if (isalpha(c))
+        {
+            if (islower(c))
+            {
                 ciphertext[i] = tolower(key[c - 'a']);
-            } else {
+            }
+            else
+            {
                 ciphertext[i] = toupper(key[c - 'A']);
             }
-        } else {
+        }
+        else
+        {
             ciphertext[i] = plaintext[i];
         }
     }
     return ciphertext;
 }
-
