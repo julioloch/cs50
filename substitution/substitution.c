171b1c53368258108f8e527a2f0f874a1ee3c47c
Julio Loch <106871296+julioloch@users.noreply.github.com>
2023-11-10 05:08:02 +0000

    Fri, Nov 10, 2023, 2:08 AM -03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -23,7 +23,7 @@ int main(int argc, string argv[])
     if (isKeyValid(key)){
         string plaintext = get_string("plaintext: ");
         string ciphertext = encrypt(plaintext, key);
-        printf("%s\n", ciphertext);
+        printf("%s", ciphertext);
     } else {
         printf("Invalid Key\n");
         return 1;
