fd9e1123d2d4ae854caf3043856bc8ff8c599f44
Julio Loch <106871296+julioloch@users.noreply.github.com>
2023-11-10 04:52:49 +0000

    Fri, Nov 10, 2023, 1:52 AM -03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -65,7 +65,7 @@ int isKeyValid(string key){
 
 string encrypt(string plaintext, string key){
 
-    string ciphertext = NULL;
+    string ciphertext = plaintext;
 
 
     for (int i = 0; plaintext[i] != '\0'; i++){
