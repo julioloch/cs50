4cdb8337a38589fc4ba2799486e923df1ee00850
Julio Loch <106871296+julioloch@users.noreply.github.com>
2023-11-10 02:25:58 +0000

    Thu, Nov 9, 2023, 11:25 PM -03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -2,17 +2,23 @@
 #include <stdio.h>
 #include <ctype.h>
 
+int isKeyValid(string key){}
+
 int main(int argc, string argv[])
 {
     if (argc = 0){
         printf("Usage: ./substitution key\n");
     } else if (argc > 1){
         printf("Key must contain 26 characters.\n");
-    } else {
-        if (strcom())
+    } else if (isKeyValid(key) > 0) {
+        if (isKeyValid())
         string key = argv[1];
     }
 
     printf("%s\n", argv[1]);
     return 0;
+}
+
+int isKeyValid(string key){
+
 }
