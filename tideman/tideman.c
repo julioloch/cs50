bb763e44cff489ca154ee2b61be1a8ae7b6df350
Julio Loch <106871296+julioloch@users.noreply.github.com>
2023-11-13 07:37:07 +0000

    Mon, Nov 13, 2023, 4:37 AM -03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -99,7 +99,7 @@ int main(int argc, string argv[])
 bool vote(int rank, string name, int ranks[])
 {
     // TODO
-    for (int i = 0; i < candidates_count; i++)
+    for (int i = 0; i < candidate_count; i++)
     {
         if (name == candidates[i])
         {
