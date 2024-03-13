3309db79dabd6e47daf8cbdb8711362031556ca0
Julio Loch <106871296+julioloch@users.noreply.github.com>
2024-03-13 04:51:25 +0000

    Wed, Mar 13, 2024, 1:51 AM -03:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,6 +1,10 @@
 #include <cs50.h>
 #include <stdio.h>
 
+//American Express uses 15-digit numbers, MasterCard uses 16-digit numbers, and Visa uses 13- and 16-digit numbers
+//All American Express numbers start with 34 or 37; most MasterCard numbers start with 51, 52, 53, 54, or 55
+//all Visa numbers start with 4
+
 int main(void)
 {
     long cardNumber;
