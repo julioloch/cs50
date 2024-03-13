0da2b60c546d5f1693f93f28e63817783ac157fa
Julio Loch <106871296+julioloch@users.noreply.github.com>
2024-03-13 06:15:14 +0000

    Wed, Mar 13, 2024, 3:15 AM -03:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,26 +1,96 @@
 #include <cs50.h>
+#include <math.h>
 #include <stdio.h>
 
-//American Express uses 15-digit numbers, MasterCard uses 16-digit numbers, and Visa uses 13- and 16-digit numbers
-//All American Express numbers start with 34 or 37; most MasterCard numbers start with 51, 52, 53, 54, or 55
-//all Visa numbers start with 4
+// variables
+bool alternateDigit = false;
 
-bool isValid(long cardNumber);
-bool checkSum(long cardNumber);
+// prototypes of functions
+bool luhn(long creditCard);
+int countDigits(long creditCard);
+
+/*
+AMEX 15 digits, starts with 34 or 37
+MasterCard 16 digits, starts with 51, 52, 53, 54 or 55
+Visa 13 and 16 digits, starts with 4
+*/
 
 int main(void)
 {
-    long cardNumber;
-    do
+    long creditCard = get_long("Number: ");
+    if (luhn(creditCard) == true)
     {
-    cardNumber = get_long("Number: ");
+        if ((countDigits(creditCard) == 15) && ((creditCard / (long) pow(10, 13) == 34) || (creditCard / (long) pow(10, 13) == 37)))
+        {
+            printf("AMEX\n");
+        }
+        else if (((countDigits(creditCard) == 13) || (countDigits(creditCard) == 16)) &&
+                 ((creditCard / (long) pow(10, 12) == 4) || (creditCard / (long) pow(10, 15) == 4)))
+        {
+            printf("VISA\n");
+        }
+        else if ((countDigits(creditCard) == 16) &&
+                 ((creditCard / (long) pow(10, 14) == 51) || (creditCard / (long) pow(10, 14) == 52) ||
+                  (creditCard / (long) pow(10, 14) == 53) || (creditCard / (long) pow(10, 14) == 54) ||
+                  (creditCard / (long) pow(10, 14) == 55)))
+        {
+            printf("MASTERCARD\n");
+        }
+        else
+        {
+            printf("INVALID\n");
+        }
     }
-    while (!isValid(cardNumber));
-
-    printf("cardNumber: %s\n", cardNumber);
+    else
+    {
+        printf("INVALID\n");
+    }
 }
 
-bool checkSum(long cardNumber)
+bool luhn(long creditCard)
 {
-    for 
+    int totalSum = 0;
+
+    while (creditCard > 0)
+    {
+        int lastDigit = creditCard % 10;
+        creditCard = creditCard / 10;
+        if (alternateDigit == true)
+        {
+            int aux = lastDigit * 2;
+            while (aux > 0) // sums each digit after multiply
+            {
+                int lastDigitAux = aux % 10;
+                aux = aux / 10;
+                totalSum += lastDigitAux;
+            }
+            alternateDigit = !alternateDigit;
+        }
+        else if (alternateDigit == false)
+        {
+            totalSum += lastDigit;
+            alternateDigit = !alternateDigit;
+        }
+    }
+
+    if (totalSum % 10 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int countDigits(long creditCard)
+{
+    int totalDigits = 0;
+    while (creditCard > 0)
+    {
+        int lastDigit = creditCard % 10;
+        creditCard = creditCard / 10;
+        totalDigits++;
+    }
+    return totalDigits;
 }
